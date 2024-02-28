98f2e703b9fc12b116312095c2ef056c5a3f4419
Muhammad Akasha <153507162+muhammad-akasha@users.noreply.github.com>
2024-02-29 01:12:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+//assigment -2-
+
+let username = " muhammad ";
+
+username = " muhammad akasha ";
+
+let myName = " akasha ";
+
+myName = " muhammad akasha ";
+
+let message;
+
+message = " hello world! ";
+
+alert(message);
+
+let student_data;
+
+name = "muhammad akasha",
+  my_age = 23,
+  education = " certified mobile and application devolopment ";
+
+alert(name);
+
+alert(my_age);
+
+alert(education);
+
+let a_tasty_pizza =
+"pizza" + "\n" + "pizz" + "\n" + "piz" + "\n" + "pi" + "\n" + "p";
+
+alert(a_tasty_pizza);
+
+let email = "my email address is";
+let email_adress = " example@example.com ";
+
+let result = email.concat(email_adress);// another method of use concat email.concat(" " ,  email_address ) it is used to add two or more string in one ..
+alert(result);
+
+console.log(result);
+
+let an_image = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+alert(an_image);
+
+let para = document.createElement("h3");
+let nodes = document.createTextNode(
+  "yeah i can write HTML content through java script"
+);
+para.append(nodes);
+let my_element = document.getElementById("mydiv");
+my_element.appendChild(para);
+document.getElementById("mydiv").style.border = "10px solid yellow";
+document.getElementById("mydiv").style.textAlign = "center";
+document.getElementById("mydiv").style.textTransform = "capitalize";
